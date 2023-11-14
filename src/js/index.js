a8954e71a740276b7bd77a5643b6a9b6f068897a
Your Name <bartlomiejczakkapi@gmail.com>
2023-11-14 20:07:05 +0100

    Add some section and navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
-console.log('hi')
+const burgerBtn = document.querySelector(".hamburger")
+const navItems = document.querySelector(".nav__items")
+
+//Nav toggle
+let isNavOpen = false
+
+const toggleNavHandler = params => {
+	if (isNavOpen === false) {
+		navItems.classList.add("nav__items--open")
+		burgerBtn.classList.add("hamburger--open")
+		isNavOpen = true
+	} else {
+		navItems.classList.remove("nav__items--open")
+		burgerBtn.classList.remove("hamburger--open")
+		isNavOpen = false
+	}
+}
+
+//Event listners
+burgerBtn.addEventListener("click", toggleNavHandler) //BurgerEvent
+ham = document.querySelector(".hamburger")
+
+ham.addEventListener("click", () => {
+	ham.classList.toggle("active")
+})
